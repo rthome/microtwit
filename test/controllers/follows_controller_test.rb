e17d993ace018cd1dccd2fb942eddb5486a8e94b
Raffael Thome <mail@rmt.name>
2015-05-29 16:53:00 +0200

    Implement following and unfollowing users with the ui

--- test/controllers/follows_controller_test.rb
+++ test/controllers/follows_controller_test.rb
@@ -2,6 +2,18 @@ require 'test_helper'
 
 class FollowsControllerTest < ActionController::TestCase
 
+  test 'create should redirect when not logged in' do
+    assert_no_difference 'Follow.count' do
+      post :create
+    end
+    assert_redirected_to login_url
+  end
 
+  test 'destroy should destroy when not logged in' do
+    assert_no_difference 'Follow.count' do
+      delete :destroy, id: follows(:one)
+    end
+    assert_redirected_to login_url
+  end
 
 end
--- test/integration/followpages_test.rb
+++ test/integration/followpages_test.rb
@@ -4,6 +4,7 @@ class FollowpagesTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:john)
+    @other_user = users(:harrison)
     log_in_as(@user)
   end
 
@@ -22,6 +23,20 @@ class FollowpagesTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select 'a[href=?]', user_path(user)
+    end
+  end
+
+  test 'should follow a user' do
+    assert_difference '@user.follows.count', 1 do
+      post follows_path, followed_id: @other_user.id
+    end
+  end
+
+  test 'should unfollow a user' do
+    @user.follow(@other_user)
+    follow = @user.follow_relationships.find_by(followed_id: @other_user.id)
+    assert_difference '@user.follows.count', -1 do
+      delete follow_path(follow)
     end
   end
 

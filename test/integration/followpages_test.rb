721973e1af60c8a0db6f337d64313d18394d6e60
Raffael Thome <mail@rmt.name>
2015-05-31 17:48:36 +0200

    99% coverage!

--- test/integration/followpages_test.rb
+++ test/integration/followpages_test.rb
@@ -8,6 +8,23 @@ class FollowpagesTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
   end
 
+  test 'feed should be on home page' do
+    get root_url
+    @user.feed.paginate(page: 1).each do |chirp|
+      assert_match CGI.escape_html(chirp.content), response.body
+    end
+  end
+
+  test 'newly created chirp should be in feed' do
+    chirp_content = "Created a new chirp at #{Time.zone.now.to_s}"
+    get root_url
+    assert_no_match chirp_content, response.body
+    post chirps_path, chirp: { content: chirp_content }
+    assert_redirected_to root_url
+    get root_url
+    assert_match chirp_content, response.body
+  end
+
   test 'follow page' do
     get follows_user_path(@user)
     assert_not @user.follows.empty?
